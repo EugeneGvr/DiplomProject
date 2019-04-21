97a456a57bcdfad0d62819028a7d5a39c61c0318
Evgenii Gavrilov <link6596@gmail.com>
2019-04-21 13:54:01 +0300

    fff

--- testForDiplom/index.php
+++ testForDiplom/index.php
@@ -2,12 +2,14 @@
 $json = file_get_contents('./koatuu.json');
 $arr = json_decode($json);
 foreach ($arr->level1 as $elementLevel1) {
-	//var_dump($value->name);
+	//var_dump($elementLevel1->name);
 	foreach ($elementLevel1->level2 as $elementLevel2) {
-	//var_dump($elementLevel2->name);
-		foreach ($elementLevel2->level3 as $elementLevel3) {
-		//var_dump($elementLevel2->name);
-		var_dump($elementLevel3);
+		if(explode(' ', $elementLevel2->name)[0] !='МІСТА' && explode(' ', $elementLevel2->name)[0] != 'РАЙОНИ') {
+			//var_dump($elementLevel2->name);
+			foreach ($elementLevel2->level3 as $elementLevel3) {
+			//var_dump($elementLevel2->name);
+			var_dump($elementLevel3);
+			}
 		}
 	}
 }
