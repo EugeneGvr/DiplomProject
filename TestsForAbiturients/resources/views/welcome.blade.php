518caad60dda0561151e38b3b67956b0a88650fe
eugenegvr <link6596@gmail.com>
2019-10-11 09:37:17 +0300

    first commit for Ivan

--- TestsForAbiturients/resources/views/welcome.blade.php
+++ TestsForAbiturients/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <h1 style="color:red;">IVAN DO DIPLOM</h1>
                 </div>
 
                 <div class="links">
